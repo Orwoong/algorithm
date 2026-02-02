e43e19ebfefa5252845b1798bb8187361bb4834e
JeongHee <music4ulove@gmail.com>
2026-02-02 14:19:44 +0900

    백준 10828번 - 스택(수정)

--- Sovled/Class2/stack(10828).py
+++ Sovled/Class2/stack(10828).py
@@ -1,8 +1,12 @@
+### 1차 제출 시간 초과로 실패 -> 원인 분석 필요
+### 2차 제출 통과 -> input()과 readline()의 속도 차이가 있음
+import sys
+
 N = int(input())
 
 stack = []
 for _ in range(N):
-    data = list(map(str, input().split()))
+    data = list(map(str, sys.stdin.readline().split()))
     if len(data) > 1:
         value = data.pop()
         stack.append(value)
